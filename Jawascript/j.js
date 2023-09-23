1943469403497d9b553f99001fdd17b4629de24b
Pritam <pmishra9500@gmail.com>
2023-09-23 19:41:50 +0530

    first commit

---
+++ Jawascript/j.js
@@ -0,0 +1,24 @@
+let input = document.querySelector('input');
+let string = "";
+let button = document.querySelectorAll('button');
+Array.from(button).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        }
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
